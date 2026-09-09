e85e259507c4b95764aad355ab4bb54710d97fa5
Lumen98 <124413077+Lumen98@users.noreply.github.com>
2026-09-09 14:32:46 -0700

    Add: count-number-of-islands - submission-4

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-4.py
@@ -0,0 +1,53 @@
+from collections import deque
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        visited = set()
+
+        res = 0
+
+        R = len(grid)
+        C = len(grid[0])
+
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] == '0':
+                    continue
+                if ((r, c)) in visited:
+                    continue
+
+                queue = deque([(r, c)])
+
+
+                while queue:
+                    row, col = queue.popleft()
+                    if (row, col) in visited:
+                        continue
+                    
+                    visited.add((row, col))
+
+                    if row + 1 < R and grid[row + 1][col] == '1':
+                        queue.append((row + 1, col))
+                    if row - 1 >= 0 and grid[row - 1][col] == '1':
+                        queue.append((row - 1, col))
+                    if col + 1 < C and grid[row][col + 1] == '1':
+                        queue.append((row, col + 1))
+                    if col - 1 >= 0 and grid[row][col - 1] == '1':
+                        queue.append((row, col - 1))
+                
+                res += 1
+        
+        return res
+
+
+
+
+
+
+
+
+
+
+        
+
+
